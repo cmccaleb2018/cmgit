bc67c42c791dbb122c1f81aea89e956c141d9fbf
cmccaleb <cmccaleb@SystemAdmin2.garealtors.local>
2018-10-10 14:56:09 -0400

    test commit

--- SI101/src/spaceInvaders/Game.java
+++ SI101/src/spaceInvaders/Game.java
@@ -9,7 +9,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferStrategy;
-import java.io.Console;
+
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
